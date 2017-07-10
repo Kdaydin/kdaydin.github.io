2c371d82d27f7cf001c28ca920e3dc5b5908ed45
Kubilay <kdaydin@gmail.com>
2017-07-10 13:46:30 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
     background(0);
-    ellipse(mouseX,mouseY,50,50);
+    ellipse(mouseX,mouseY,random(50),random(50));
     stroke(250,0,150);
     fill(150,100,120);
 }
