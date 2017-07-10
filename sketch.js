2f010d4b0ab68cab72488991167e451f1564dc31
serdarsam <serdar.samancioglu@intertech.com.tr>
2017-07-10 14:12:00 +0300

    draw func. modified

--- sketch.js
+++ sketch.js
@@ -1,13 +1,22 @@
+var red_val=50;
+var green_val=100;
+var blue_val=150;
+
 function setup() {
     createCanvas(800, 700);
     background(0);
+    
 }
 
 function draw() {
     
     noStroke();
-    fill(150,100,120,30);
+    
+    fill((red_val%255),(green_val%255),(blue_val%255),50);
     ellipse(mouseX,mouseY,25,25);
+    red_val+=random(10);
+    green_val+=random(20);
+    blue_val+=random(30);
     
 }
 
