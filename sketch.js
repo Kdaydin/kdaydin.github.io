0b3a5a0ecc890da1ff423903aa49ab02c06f9a61
Kubilay <kdaydin@gmail.com>
2017-07-10 13:43:31 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 800);
 }
 
 function draw() {
     background(0);
-    ellipse(200,200,50,50);
+    ellipse(mouseX,mouseY,50,50);
     stroke(250,0,150);
     fill(150,100,120);
 }
